68c78223ffa6dd55d225fcef1222e0091e6c7cc7
BuyWell <maksim-d2013@mail.ru>
2024-12-22 23:00:12 +0300

    v.1.1.3

--- src/app/NoSSRWrapper.tsx
+++ src/app/NoSSRWrapper.tsx
@@ -1,8 +1,6 @@
-import dynamic from 'next/dynamic'
-import React from 'react' 
-const NoSSRWrapper = props => ( 
-    <React.Fragment>{props.children}</React.Fragment> 
-) 
-export default dynamic(() => Promise.resolve(NoSSRWrapper), { 
-    ssr: false 
-})
+import dynamic from 'next/dynamic';
+import React, { FC } from 'react';
+const NoSSRWrapper: FC = (props: React.PropsWithChildren<{}>) => <React.Fragment>{props.children}</React.Fragment>;
+export default dynamic(() => Promise.resolve(NoSSRWrapper), {
+  ssr: false,
+});
